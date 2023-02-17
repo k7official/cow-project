913d962501d1f93e85cf419f62d99295ae11264d
k7official <78650685+k7official@users.noreply.github.com>
2023-02-17 20:51:38 +0800

    created database schema

--- main.py
+++ main.py
@@ -8,8 +8,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 class Cow(db.Model):
-    id_tag = db.Column(db.Integer, primary_key=True)
+    id_tag = db.Column(db.String(100), primary_key=True)
+    ear_tag = db.Column(db.String(100), unique=True, nullable=False)
+    animal_type = db.Column(db.String(250), nullable=False)
+    breed = db.Column(db.String(250), nullable=False)
+    color = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
 
 
 @app.route('/')
