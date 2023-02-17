e9d7367ccd3196ac9f5f998c55a0572355a0c0e4
k7official <78650685+k7official@users.noreply.github.com>
2023-02-18 00:48:41 +0800

    initialized database

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ db = SQLAlchemy(app)
 
 
 class Cow(db.Model):
-    id_tag = db.Column(db.String(100), primary_key=True)
+    animal_id = db.Column(db.String(100), primary_key=True)
     ear_tag = db.Column(db.String(100), unique=True, nullable=False)
     animal_type = db.Column(db.String(250), nullable=False)
     breed = db.Column(db.String(250), nullable=False)
@@ -28,6 +28,28 @@ def home():
 @app.route('/identify', methods=["GET", "POST"])
 def identify():
     return render_template("identify.html")
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        new_cow = Cow(
+            animal_id=request.form["animal_id"],
+            ear_tag=request.form["ear_tag"],
+            animal_type=request.form["animal_type"],
+            breed=request.form["breed"],
+            color=request.form["color"],
+            image=request.form["image"]
+        )
+        with app.app_context():
+            db.create_all()
+            # Add record
+            db.session.add(new_cow)
+            db.session.commit()
+
+        return redirect(url_for('home'))
+
+    return render_template("add_animal.html")
 
 
 if __name__ == "__main__":
