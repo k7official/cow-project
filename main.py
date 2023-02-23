2d9b654bc77698892cadec6390ce69ca82dd80d5
k7official <78650685+k7official@users.noreply.github.com>
2023-02-24 01:32:15 +0800

    still working with images

--- main.py
+++ main.py
@@ -16,10 +16,17 @@ class Cow(db.Model):
     animal_type = db.Column(db.String(250), nullable=False)
     breed = db.Column(db.String(250), nullable=False)
     color = db.Column(db.String(250), nullable=False)
-    image = db.LargeBinary()
 
     def __repr__(self):
         return f'<Cow {self.title}>'
+
+
+class Img(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pic = db.Column(db.LargeBinary)
+
+    def __repr__(self):
+        return f'<Img {self.title}>'
 
 
 @app.route('/')
@@ -35,25 +42,33 @@ def identify():
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        if 'image' not in request.files:
-            # Handle case where no image is uploaded
-            pass
-        else:
-            image_data = request.files['image'].read()
-            encoded_image = base64.b64encode(image_data)
-            new_cow = Cow(
-                animal_id=request.form["animal_id"],
-                ear_tag=request.form["ear_tag"],
-                animal_type=request.form["animal_type"],
-                breed=request.form["breed"],
-                color=request.form["color"],
-                image=encoded_image
-            )
-            with app.app_context():
-                db.create_all()
-                # Add record
-                db.session.add(new_cow)
-                db.session.commit()
+        # if 'pic' not in request.files:
+        #     # Handle case where no image is uploaded
+        #     pass
+        # else:
+        #     # image_data = request.files['image'].read()
+        #     # encoded_image = base64.b64encode(image_data)
+        new_cow = Cow(
+            animal_id=request.form["animal_id"],
+            ear_tag=request.form["ear_tag"],
+            animal_type=request.form["animal_type"],
+            breed=request.form["breed"],
+            color=request.form["color"],
+            # image=encoded_image
+        )
+        file = request.files['pic'].read()
+        # print(file)
+        new_img = Img(pic=file)
+        # Add record
+        db.session.add(new_cow)
+        db.session.add(new_img)
+        try:
+            db.session.commit()
+        except Exception as e:
+            print(f"Error adding data to database: {e}")
+            db.session.rollback()
+        finally:
+            db.session.close()
 
         return redirect(url_for('home'))
 
@@ -61,4 +76,7 @@ def add():
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  # Create all tables before running the app
     app.run(debug=True)
+
